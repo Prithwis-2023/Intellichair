555f5d84a780b6fb99c00acb9061173b5489d421
Prithwis-2023 <prithwisdas2017@gmail.com>
2025-08-14 20:57:58 +0900

    first commit

---
+++ Arduino/Arduino.ino
@@ -0,0 +1,22 @@
+// Imports
+#include "HX711.h"
+
+// HX711 to Arduino wiring
+const int LOADCELL1_DOUT_PIN = 2;
+const int LOADCELL2_DOUT_PIN = 3;
+const int LOADCELL3_DOUT_PIN = 4;
+const int LOADCELL4_DOUT_PIN = 5;
+const int LOADCELL1_SCK_PIN = 9;
+const int LOADCELL2_SCK_PIN = 10;
+const int LOADCELL3_SCK_PIN = 11;
+const int LOADCELL4_SCK_PIN = 12;
+
+// piezo wiring
+//TBD
+
+// Initialization
+HX711 Scale;
+
+void setup() {}
+
+void loop() {}
