163ee26b31532f00da0321bcce990e43a04f53d2
Prithwis-2023 <prithwisdas2017@gmail.com>
2025-08-15 15:11:47 +0900

    update

--- Arduino/Arduino.ino
+++ Arduino/Arduino.ino
@@ -12,11 +12,26 @@ const int LOADCELL3_SCK_PIN = 11;
 const int LOADCELL4_SCK_PIN = 12;
 
 // piezo wiring
+
 //TBD
 
 // Initialization
 HX711 Scale;
 
-void setup() {}
+void setup() 
+{
+  Serial.begin(9600);
+  Scale.begin(LOADCELL1_DOUT_PIN, LOADCELL1_SCK_PIN);
+  Scale.begin(LOADCELL2_DOUT_PIN, LOADCELL2_SCK_PIN);
+  Scale.begin(LOADCELL3_DOUT_PIN, LOADCELL3_SCK_PIN);
+  Scale.begin(LOADCELL4_DOUT_PIN, LOADCELL4_SCK_PIN);
+  pinMode(A0, OUTPUT);  
+}
 
-void loop() {}
+void loop() 
+{
+  digitalWrite(A0, HIGH);  // Turn buzzer ON
+  delay(500);
+  digitalWrite(A0, LOW);   // Turn buzzer OFF
+  delay(500);
+}
