30a71461b08799c1cded38b3066949583928465d
Prithwis-2023 <prithwisdas2017@gmail.com>
2025-08-18 16:28:51 +0900

    uypdate

--- Arduino/Arduino.ino
+++ Arduino/Arduino.ino
@@ -2,81 +2,78 @@
 #include "HX711.h"
 
 // Init and HX711 to Arduino wiring
-HX711 Scale1;
-HX711 Scale2;
-HX711 Scale3;
-HX711 Scale4;
+HX711 Scale1, Scale2, Scale3, Scale4;
 
-const int LOADCELL1_DOUT_PIN = 2;
-const int LOADCELL2_DOUT_PIN = 3;
+uint8_t LOADCELL_DOUT_PINS[4] = {2, 3, 4, 5};
+uint8_t LOADCELL_SCK_PINS[4] = {9, 10, 11, 12};
 
-const int LOADCELL3_DOUT_PIN = 4;
-const int LOADCELL4_DOUT_PIN = 5;
-
-const int LOADCELL1_SCK_PIN = 9;
-const int LOADCELL2_SCK_PIN = 10;
-
-const int LOADCELL3_SCK_PIN = 11;
-const int LOADCELL4_SCK_PIN = 12;
-
-// const float calibration_factor1 = ;
-// const float calibration_factor2 = ;
-// const float calibration_factor3 = ;
-// const float calibration_factor4 = ;
-
-const float imbalanceRatio = 0.1; // variable
+//const float calibration_factors[4] = {};
 
 // Buzzer pin (passive buzzer)
 const int buzzerPin = A0;
+
+// SI thresholds
+const float SI_SOFT_LIMIT = 10;  // for a mild warning
+const float SI_HARD_LIMIT = 20;  // for a strong warning
+
 
 
 void setup() 
 {
   Serial.begin(9600);
-  Scale1.begin(LOADCELL1_DOUT_PIN, LOADCELL1_SCK_PIN);
-  Scale2.begin(LOADCELL2_DOUT_PIN, LOADCELL2_SCK_PIN);
-  Scale3.begin(LOADCELL3_DOUT_PIN, LOADCELL3_SCK_PIN);
-  Scale4.begin(LOADCELL4_DOUT_PIN, LOADCELL4_SCK_PIN);
+  calibrateCell (197, 0);
+  calibrateCell (197, 1);
+  calibrateCell (197, 2);
+  calibrateCell (197, 3);
+
+  Scale1.begin(LOADCELL_DOUT_PINS[0], LOADCELL_SCK_PINS[0]);
+  Scale2.begin(LOADCELL_DOUT_PINS[1], LOADCELL_SCK_PINS[1]);
+  Scale3.begin(LOADCELL_DOUT_PINS[2], LOADCELL_SCK_PINS[2]);
+  Scale4.begin(LOADCELL_DOUT_PINS[3], LOADCELL_SCK_PINS[3]);
 
   pinMode(buzzerPin, OUTPUT); 
 
+
   // set the calibration factor and reset the scale to 0 using the tare() function
-  Scale1.set_scale(); 
-  Scale1.tare();
+  // Scale1.set_scale(); Scale1.tare();
+  // Scale2.set_scale(); Scale2.tare();
+  // Scale3.set_scale(); Scale3.tare();
+  // Scale4.set_scale(); Scale4.tare();        
 
-  Scale2.set_scale();
-  Scale2.tare();
-
-  Scale3.set_scale();
-  Scale3.tare();
-
-  Scale4.set_scale();
-  Scale4.tare();        
 }
 
 void loop() 
 {
-  // Read values (raw units) on an average of 10 reads
-  long w1 = Scale1.get_units(10);
-  long w2 = Scale2.get_units(10);
-  long w3 = Scale3.get_units(10);
-  long w4 = -Scale4.get_units(10);  // inverted the 4th load cell
+  // // Read values (raw units) on an average of 10 reads
+  // long w1 = Scale1.get_units(10);
+  // long w2 = Scale2.get_units(10);
+  // long w3 = Scale3.get_units(10);
+  // long w4 = -Scale4.get_units(10);  // inverted the 4th load cell
 
-  long totalWeight = w1 + w2 + w3 + w4;
+  // float total = w1 + w2 + w3 + w4;
 
-  long leftSide = w2 + w3; long rightSide = w1 + w4;
+  // float leftSide = w1 + w4; float rightSide = w2 + w3;
 
-  if (abs(leftSide - rightSide) >= totalWeight * imbalanceRatio)
-  {
-    tone(A0, 200);
-  }
-  else
-  {
-    noTone(A0);
-  }
+  // // ----- Symmetry Index (SI) (left-right) - absolute percent
+  // // SI = 2 * (X_d - X_nd) / (|X_d| + |X_nd|) * 100
+  // // we'll compute absolute percent (0..inf) as a positive number
+  // float SI_lr = 0.0;
+  // if (fabs(leftSide) + fabs(rightSide) > 1e-6) 
+  // {
+  //   SI_lr = fabs(2.0 * (rightSide - leftSide) / (fabs(rightSide) + fabs(leftSide)) * 100.0);
+  // }
 
-  Serial.print("W1: "); Serial.print(w1);
-  Serial.print(" W2: "); Serial.print(w2);
-  Serial.print(" W3: "); Serial.print(w3);
-  Serial.print(" W4: "); Serial.println(w4);    
+  // if (SI_lr >= SI_HARD_LIMIT)
+  // {
+  //   //Serial.println(SI_lr);
+  // }
+  // else 
+  // {
+  //   noTone(buzzerPin);
+  // }
+
+  // // Serial.print("W1: "); Serial.print(w1);
+  // // Serial.print(" W2: "); Serial.print(w2);
+  // // Serial.print(" W3: "); Serial.print(w3);
+  // // Serial.print(" W4: "); Serial.println(w4);    
 }
