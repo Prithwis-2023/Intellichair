aaa93d9d84c96e7cc41225f519cbe6a3fa78630d
Prithwis-2023 <prithwisdas2017@gmail.com>
2025-08-27 18:01:55 +0900

    update

--- Arduino/Arduino.ino
+++ Arduino/Arduino.ino
@@ -2,7 +2,7 @@
 #include "Arduino_BMI270_BMM150.h"
 #include <ArduinoBLE.h>
 
-#define MINIMUM_TILT 20
+#define MINIMUM_TILT 5
 
 float x, y, z;
 int angleX = 0;
@@ -21,7 +21,7 @@ const int buzzerPin = 6;  // passive buzzer
 HX711 scaleFR, scaleBR, scaleBL, scaleFL; 
 
 BLEService alertService("180A");  // custom UUID
-BLEByteCharacteristic alertCharacteristic("2A57", BLERead | BLENotify);
+BLECharacteristic alertCharacteristic("2A57", BLERead | BLENotify, 50);
 
 void setup() 
 {
@@ -61,7 +61,10 @@ void setup()
   alertService.addCharacteristic(alertCharacteristic);
   BLE.addService(alertService);
 
-  alertCharacteristic.writeValue(0); // initial value
+  //alertCharacteristic.writeValue(0); // initial value
+  // const char* initMsg = "NORMAL!";
+  // alertCharacteristic.writeValue((const unsigned char*)initMsg, strlen(initMsg));
+
 
   BLE.advertise();
   Serial.println("BLE device is now advertising...");
@@ -69,43 +72,55 @@ void setup()
 
 void loop() 
 {
-  BLE.poll();
-
-  if (IMU.accelerationAvailable() && scaleFR.is_ready() && scaleBR.is_ready() && scaleBL.is_ready() && scaleFL.is_ready()) 
+  BLEDevice central = BLE.central();
+  if (central)
   {
-    IMU.readAcceleration(x, y, z);
-    angleY = atan2(y, sqrt(x*x + z*z)) * 180 / PI;
-    angleX = atan2(x, sqrt(y * y + z * z)) * 180 / PI;
+    Serial.print("Connected to central: ");
+    Serial.println(central.address());
 
-    float readingFR = fabs(scaleFR.get_units(10));
-    float readingBR = fabs(scaleBR.get_units(10));
-    float readingBL = fabs(scaleBL.get_units(10));
-    float readingFL = fabs(scaleFL.get_units(10));
-    float total = readingFR + readingBR + readingBL + readingFL;
-    
-    Serial.print("HX711 reading: ");
-
-    float rightSideNorm = (readingFR + readingBR) / total; 
-    float leftSideNorm = (readingBL + readingFL) / total;
-    Serial.print(leftSideNorm); Serial.print(" "); Serial.println(rightSideNorm);
-
-    float SI = fabs(rightSideNorm - leftSideNorm) * 100.0f;
-    float CoPx = (chassisLength * 0.5f) * fabs(rightSideNorm - leftSideNorm);
-    Serial.println(SI);
-    Serial.println(CoPx);
-
-    bool alert = ((SI >= SI_THRESHOLD && CoPx >= CoPx_THRESHOLD) || (angleY >= MINIMUM_TILT || angleX >= MINIMUM_TILT) || (angleY <= -MINIMUM_TILT || angleX <= -MINIMUM_TILT))
-    if (alert)
+    while (central.connected())
     {
-      tone(buzzerPin, 1000);
-      const char* msg = "ALERT! CHAIR FALLING!";
-      alertCharacteristic.writeValue((const unsigned char*)msg, strlen(msg)); // alert
+      BLE.poll();
+
+      if (IMU.accelerationAvailable() && scaleFR.is_ready() && scaleBR.is_ready() && scaleBL.is_ready() && scaleFL.is_ready()) 
+      {
+        IMU.readAcceleration(x, y, z);
+        angleY = atan2(y, sqrt(x*x + z*z)) * 180 / PI;
+        angleX = atan2(x, sqrt(y * y + z * z)) * 180 / PI;
+
+        float readingFR = fabs(scaleFR.get_units(10));
+        float readingBR = fabs(scaleBR.get_units(10));
+        float readingBL = fabs(scaleBL.get_units(10));
+        float readingFL = fabs(scaleFL.get_units(10));
+        float total = readingFR + readingBR + readingBL + readingFL;
+        
+        Serial.print("HX711 reading: ");
+
+        float rightSideNorm = (readingFR + readingBR) / total; 
+        float leftSideNorm = (readingBL + readingFL) / total;
+        Serial.print(leftSideNorm); Serial.print(" "); Serial.println(rightSideNorm);
+
+        float SI = fabs(rightSideNorm - leftSideNorm) * 100.0f;
+        float CoPx = (chassisLength * 0.5f) * fabs(rightSideNorm - leftSideNorm);
+        Serial.println(SI);
+        Serial.println(CoPx);
+
+        bool alert = ((SI >= SI_THRESHOLD && CoPx >= CoPx_THRESHOLD) || (angleY >= MINIMUM_TILT || angleX >= MINIMUM_TILT) || (angleY <= -MINIMUM_TILT || angleX <= -MINIMUM_TILT));
+        if (alert)
+        {
+          tone(buzzerPin, 1000);
+          if ((angleY >= MINIMUM_TILT || angleX >= MINIMUM_TILT) || (angleY <= -MINIMUM_TILT || angleX <= -MINIMUM_TILT))
+          {
+            alertCharacteristic.setValue("DANGER!");
+          }
+        }
+        else
+        {
+          noTone(buzzerPin);
+        }
+        
+      } 
+      delay(1000); 
     }
-    else
-    {
-      noTone(buzzerPin);
-      //alertCharacteristic.writeValue(0); // no alert
-    }
-  } 
-  delay(1000); 
+  }
 }
