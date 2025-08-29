a0d724005f02505227b52b5f75853d03451483a2
Prithwis Das <prithwisdas2017@gmail.com>
2025-08-29 11:52:52 +0900

    updates

--- Dummy_Load_CellCode_copy_20250828222639/Dummy_Load_CellCode_copy_20250828222639.ino
+++ Dummy_Load_CellCode_copy_20250828222639/Dummy_Load_CellCode_copy_20250828222639.ino
@@ -6,11 +6,16 @@
 #define FR_SCK_PIN 9
 #define BR_DT_PIN 3
 #define BR_SCK_PIN 10
-#define SI_THRESHOLD 90
+#define CELL1_DT_PIN 4
+#define CELL1_SCK_PIN 11
+#define CELL2_DT_PIN 5
+#define CELL2_SCK_PIN 12
+#define SI_MAX_THRESHOLD 80
+#define SI_MIN_THRESHOLD 10
 
-HX711 sFR, sBR;  // Front Right and Back Right Load Cells
+HX711 sFR, sBR, s1, s2;  // Front Right and Back Right Load Cells
 
-const float CALIBRATIONS[4] = {954.76, -291.17, 396.39, 985.72};  // Calibration values for the load cells
+const float CALIBRATIONS[4] = {954.76, -291.17, 26.11, 16.60};  // Calibration values for the load cells
 
 const int buzzerPin = 6;
 
@@ -31,27 +36,37 @@ void setup()
 
   sFR.begin(FR_DT_PIN, FR_SCK_PIN);
   sBR.begin(BR_DT_PIN, BR_SCK_PIN);
+  s1.begin(CELL1_DT_PIN, CELL1_SCK_PIN);
+  s2.begin(CELL2_DT_PIN, CELL2_SCK_PIN);
+  
 
   sFR.tare();
   sBR.tare();
+  s1.tare();
+  s2.tare();
 
   sFR.set_scale(CALIBRATIONS[0]);
   sBR.set_scale(CALIBRATIONS[1]);
+  s1.set_scale(CALIBRATIONS[2]);
+  s2.set_scale(CALIBRATIONS[3]);
 
   pinMode(buzzerPin, OUTPUT);
 }
 
 void loop()
 {
-  if (IMU.accelerationAvailable() && sFR.is_ready() && sBR.is_ready())
+  if (IMU.accelerationAvailable() && sFR.is_ready() && sBR.is_ready() && s1.is_ready() && s2.is_ready())
   {
    /* Load Cell Analysis */ 
     long rFR = fabs(sFR.get_units());
     long rBR = fabs(sBR.get_units());
-    float total = rFR + rBR;
-    float leftNorm = rBR / total;  float rightNorm = rFR / total;
+    long r1 = fabs(s1.get_units());
+    long r2 = fabs(s2.get_units());
+    float total = rFR + rBR + r1 + r2;
+    float leftNorm = (rBR + r1) / total;  float rightNorm = (rFR + r2) / total;
     Serial.print(leftNorm); Serial.print(" "); Serial.println(rightNorm);  //
     float SI = fabs(rightNorm - leftNorm) * 100.0f; 
+    Serial.println(SI);
     float CoPx = (300 * 0.5f) * fabs(rightNorm - leftNorm);
 
     /* Accelerometer Analysis */
@@ -60,7 +75,7 @@ void loop()
     angleX = atan2(x, sqrt(y * y + z * z)) * 180 / PI;
 
     /* If the chair is falling or the person is slouching, buzz*/
-    if ((SI > SI_THRESHOLD) || (angleY > 5 || angleY <-5) || (angleX > 5 || angleX < -5))
+    if ((SI > SI_MAX_THRESHOLD) || (SI <= SI_MIN_THRESHOLD) || (angleY > 5 || angleY <-5) || (angleX > 5 || angleX < -5))
     {
       tone(buzzerPin, 500);
       delay(1000);
