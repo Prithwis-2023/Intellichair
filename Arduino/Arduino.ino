7230ec2c8785640629c2151f24d85228d6546be8
Prithwis Das <prithwisdas2017@gmail.com>
2025-08-29 14:24:17 +0900

    update

--- Arduino/Arduino.ino
+++ Arduino/Arduino.ino
@@ -1,46 +1,34 @@
-#include "HX711.h"
+#include <HX711.h>
 #include "Arduino_BMI270_BMM150.h"
 #include <ArduinoBLE.h>
 
-#define MINIMUM_TILT 5
+#define FR_DT_PIN 2
+#define FR_SCK_PIN 9
+#define BR_DT_PIN 3
+#define BR_SCK_PIN 10
+#define CELL1_DT_PIN 4
+#define CELL1_SCK_PIN 11
+#define CELL2_DT_PIN 5
+#define CELL2_SCK_PIN 12
+#define SI_MAX_THRESHOLD 80
+#define SI_MIN_THRESHOLD 10
 
-//FR: 919.43  FL: 396.39  BR: -271.69  BL: 985.72
+HX711 sFR, sBR, s1, s2;  // Front Right and Back Right Load Cells
 
-// void setup()
-// {
-//   // Serial.begin(57600);
-//   // while (!Serial); // wait for serial connection
-//   // scaleBR.begin(2, 9);
-  
-//   // //scaleBR.tare(10);
-//   // Serial.print("Calibration for BR: ");
-//   // scaleBR.calibrate_scale(500);
-//   // Serial.println(scaleBR.get_scale());
-// }
+const float CALIBRATIONS[4] = {954.76, -291.17, 26.11, 16.60};  // Calibration values for the load cells
 
-float x, y, z;
-int angleX = 0;
-int angleY = 0;
+const int buzzerPin = 6;
 
-// HX711 circuit wiring
-const int LOADCELL_DOUT_PINS[4] = {3, 2, 5, 4};
-const int LOADCELL_SCK_PINS[4] = {10, 9, 12, 11};
-const float CALIBRATIONS[4] = {919.43, -271.69, 396.39, 985.72};  // accomplished based on the documentation: github.com/RobTillaart/HX711/tree/master
-const int chassisLength = 300;  // TBD
-const int SI_THRESHOLD = 15;
-const int CoPx_THRESHOLD = 30;  // in mm
-
-const int buzzerPin = 6;  // passive buzzer
-
-HX711 scaleFR, scaleBR, scaleBL, scaleFL; 
+float x, y, z;    // Variables to hold accelerometer data
+int angleX = 0;  // Variable to hold calculated angles
+int angleY = 0; // Variable to hold calculated angles
 
 BLEService alertService("180A");  // custom UUID
 BLECharacteristic alertCharacteristic("2A57", BLERead | BLENotify, 50);
 
-void setup() 
+void setup()
 {
-  Serial.begin(57600);  // suggested baud rate online
-
+  Serial.begin(9600);
   while(!Serial);
 
   if (!IMU.begin())
@@ -48,101 +36,64 @@ void setup()
     Serial.println("Failed to initialize IMU!");
     while (1);
   }
-  
-  // Init HX711
-  scaleFR.begin(LOADCELL_DOUT_PINS[0], LOADCELL_SCK_PINS[0]);
-  scaleBR.begin(LOADCELL_DOUT_PINS[1], LOADCELL_SCK_PINS[1]);
-  scaleFL.begin(LOADCELL_DOUT_PINS[2], LOADCELL_SCK_PINS[2]);
-  scaleBL.begin(LOADCELL_DOUT_PINS[3], LOADCELL_SCK_PINS[3]);
 
-  scaleFR.tare(10); scaleBR.tare(10); scaleFL.tare(10); scaleBL.tare(10);
+  sFR.begin(FR_DT_PIN, FR_SCK_PIN);
+  sBR.begin(BR_DT_PIN, BR_SCK_PIN);
+  s1.begin(CELL1_DT_PIN, CELL1_SCK_PIN);
+  s2.begin(CELL2_DT_PIN, CELL2_SCK_PIN);
   
-  // calibrate the scales
-  scaleFR.set_scale(CALIBRATIONS[0]);
-  scaleBR.set_scale(CALIBRATIONS[1]);
-  scaleFL.set_scale(CALIBRATIONS[2]);
-  scaleBL.set_scale(CALIBRATIONS[3]);
-  
+
+  sFR.tare();
+  sBR.tare();
+  s1.tare();
+  s2.tare();
+
+  sFR.set_scale(CALIBRATIONS[0]);
+  sBR.set_scale(CALIBRATIONS[1]);
+  s1.set_scale(CALIBRATIONS[2]);
+  s2.set_scale(CALIBRATIONS[3]);
 
   pinMode(buzzerPin, OUTPUT);
-
-  // Init BLE
-  if (!BLE.begin())
-  {
-    Serial.println("Starting BLE failed!");
-    while (1);
-  }
 
   BLE.setLocalName("TiltMonitor");
   BLE.setAdvertisedService(alertService);
   alertService.addCharacteristic(alertCharacteristic);
   BLE.addService(alertService);
 
-  //alertCharacteristic.writeValue(0); // initial value
-  // const char* initMsg = "NORMAL!";
-  // alertCharacteristic.writeValue((const unsigned char*)initMsg, strlen(initMsg));
-
-
-  BLE.advertise();
-  Serial.println("BLE device is now advertising...");
 }
 
-void loop() 
+void loop()
 {
-  BLEDevice central = BLE.central();
-  if (central)
+  if (IMU.accelerationAvailable() && sFR.is_ready() && sBR.is_ready() && s1.is_ready() && s2.is_ready())
   {
-    Serial.print("Connected to central: ");
-    Serial.println(central.address());
+   /* Load Cell Analysis */ 
+    long rFR = fabs(sFR.get_units());
+    long rBR = fabs(sBR.get_units());
+    long r1 = fabs(s1.get_units());
+    long r2 = fabs(s2.get_units());
+    float total = rFR + rBR + r1 + r2;
+    float leftNorm = (rBR + r1) / total;  float rightNorm = (rFR + r2) / total;
+    Serial.print(leftNorm); Serial.print(" "); Serial.println(rightNorm);  //
+    float SI = fabs(rightNorm - leftNorm) * 100.0f; 
+    Serial.println(SI);
+    float CoPx = (300 * 0.5f) * fabs(rightNorm - leftNorm);
 
-    while (central.connected())
+    /* Accelerometer Analysis */
+    IMU.readAcceleration(x, y, z);
+    angleY = atan2(y, sqrt(x*x + z*z)) * 180 / PI;
+    angleX = atan2(x, sqrt(y * y + z * z)) * 180 / PI;
+
+    /* If the chair is falling or the person is slouching, buzz*/
+    if ((SI > SI_MAX_THRESHOLD) || (SI <= SI_MIN_THRESHOLD) || (angleY > 5 || angleY <-5) || (angleX > 5 || angleX < -5))
     {
-      BLE.poll();
-
-      if (IMU.accelerationAvailable() && scaleFR.is_ready() && scaleBR.is_ready() && scaleBL.is_ready() && scaleFL.is_ready()) 
-      {
-        IMU.readAcceleration(x, y, z);
-        angleY = atan2(y, sqrt(x*x + z*z)) * 180 / PI;
-        angleX = atan2(x, sqrt(y * y + z * z)) * 180 / PI;
-
-        float readingFR = fabs(scaleFR.get_units(10));
-        float readingBR = fabs(scaleBR.get_units(10));
-        float readingBL = fabs(scaleBL.get_units(10));
-        float readingFL = fabs(scaleFL.get_units(10));
-        float total = readingFR + readingBR + readingBL + readingFL;
-        Serial.println(readingFR);
-
-        Serial.print("Total: ");
-        Serial.println(total);
-        Serial.print("HX711 reading: ");
-
-        float rightSideNorm = (readingFR + readingBR) / total; 
-        float leftSideNorm = (readingBL + readingFL) / total;
-        Serial.print(leftSideNorm); Serial.print(" "); Serial.println(rightSideNorm);
-
-        float SI = fabs(rightSideNorm - leftSideNorm) * 100.0f;
-        float CoPx = (chassisLength * 0.5f) * fabs(rightSideNorm - leftSideNorm);
-        //Serial.println(SI);
-        //Serial.println(CoPx);
-
-        bool alert = ((SI >= SI_THRESHOLD || CoPx >= CoPx_THRESHOLD));
-        if (alert)
-        {
-          //tone(buzzerPin, 1000);
-          //Serial.println(SI);
-          //Serial.println(CoPx);
-          // if ((angleY >= MINIMUM_TILT || angleX >= MINIMUM_TILT) || (angleY <= -MINIMUM_TILT || angleX <= -MINIMUM_TILT))
-          // {
-          //   alertCharacteristic.setValue("DANGER!");
-          // }
-        }
-        else
-        {
-          noTone(buzzerPin);
-        }
-        
-      } 
-      delay(1000); 
+      tone(buzzerPin, 2000);
+      delay(1000);
+      noTone(buzzerPin);
+      delay(1000);
+    }
+    else
+    {
+      noTone(buzzerPin);
     }
   }
 }
